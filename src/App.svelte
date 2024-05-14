8b43b12ec3a7819d844663285b5555015be62e45
c26ns <c26ns@dalton.org>
2024-05-14 12:13:53 -0400

    Updated styling, started player selection

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,15 @@
 
 <script>
   import { onMount } from "svelte";
-  import { initializeApp } from "firebase/app";
-  import {
-    getFirestore,
-    collection,
-    getDoc,
-    doc,
-    onSnapshot,
-    setDoc,
-  } from "firebase/firestore";
+  // import { initializeApp } from "firebase/app";
+  // import {
+  //   getFirestore,
+  //   collection,
+  //   getDoc,
+  //   doc,
+  //   onSnapshot,
+  //   setDoc,
+  // } from "firebase/firestore";
 
   // let wordGrid = [];
   // const boardsize = 5;
@@ -32,7 +32,7 @@
   let hands = [[], [], [], []];
   onMount(async () => {
     // get a deck
-    let deck = [
+    const deck = [
       {suit: "D", number: 2},{suit: "D",number: 3},{suit: "D", number:4},{suit: "D", number:5},{suit: "D", number:6},{suit: "D", number:7},{suit: "D", number:8},{suit: "D", number:9},{suit: "D", number:10},{suit: "D", number:11},{suit: "D", number:12},{suit: "D", number:13},{suit: "D", number: 14},
       {suit: "H", number: 2},{suit: "H", number: 3},{suit: "H", number: 4},{suit: "H", number: 5},{suit: "H", number: 6},{suit: "H", number: 7},{suit: "H", number: 8},{suit: "H", number: 9},{suit: "H", number: 10},{suit: "H", number: 11},{suit: "H", number: 12},{suit: "H", number: 13},{suit: "H", number: 14},
       {suit: "S", number: 2},{suit: "S", number: 3},{suit: "S", number: 4},{suit: "S", number: 5},{suit: "S", number: 6},{suit: "S", number: 7},{suit: "S", number: 8},{suit: "S", number: 9},{suit: "S", number: 10},{suit: "S", number: 11},{suit: "S", number: 12},{suit: "S", number: 13},{suit: "S", number: 14},
@@ -45,7 +45,6 @@
         deck.splice(index, 1);
       }
     }
-    console.log(hands);
     hands = hands;
     
   });
@@ -55,11 +54,15 @@
   <h1>Hearts Online Arena</h1>
 
   <div class="trick"></div>
+  <div class="playerselect">
+    
+  </div>
+  <h3>Your Hand:</h3>
   <div class="hand">
     {#each hands[0] as card}
-      <div class="grid-item">
+      <button class="grid-item {card.suit === "S" || card.suit === "C" ? "black" : "red" }">
         {card.number} {card.suit}
-      </div>
+      </button>
     {/each}
   </div>
 </main>
@@ -69,6 +72,15 @@
     display: grid;
     grid-template-columns: repeat(5, 1fr);
     gap: 10px;
-    justify-content: center;
+  }
+  .grid-item {
+    background-color: aliceblue;
+    border: 2px solid navy;
+  }
+  .black {
+    color: black;
+  }
+  .red {
+    color: red
   }
 </style>
